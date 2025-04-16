6791520a95cf54d28f48fb0de563d9a507118a8b
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2025-04-16 13:08:34 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,8 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <>
-      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <h1><a className="navbar-brand" href="/">{props.title}</a></h1>
-        <div className="form-check form-switch ">
-            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-            <label className="form-check-label" htmlfor="flexSwitchCheckDefault">Enable DarkMode</label>
-          </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
        
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
