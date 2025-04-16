33ee8a654ee3285a103b20d1ab110814df04a565
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2025-04-16 13:20:18 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,12 +13,7 @@ export default function Navbar(props) {
             <li className="nav-item active">
               <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-          </form>
-         
+          </ul>    
         </div>
       </nav>
     </>
