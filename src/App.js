2a9530783a183f2befdd7907748d4221b53582a9
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2025-04-16 12:55:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,43 +2,34 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import Alert from './components/Alert';
 import About from './components/About';
 //import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState("light");
-  const [alert, setAlert] = useState(null);
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type
-    })
-    setTimeout(() => {
-      setAlert(null);
-    }, 1000);
-  }
+  
+
   const toggleMode = () => {
     if (mode === "light") {
       setmode("dark");
       document.body.style.backgroundColor = '#07041a';
       document.body.style.color = 'white';
-      showAlert("Dark Mode Enabled");
+    
     } else {
       setmode("light");
       document.body.style.backgroundColor = 'gray';
       document.body.style.color = 'black';
-      showAlert("Light Mode Enable");
+    
     }
   }
   return (
 
     <>
       <Navbar title="EdiText" mode={mode} toggleMode={toggleMode}></Navbar>
-      <div style={{height:'30px'}}><Alert alert={alert}></Alert></div>
+      
 
       <div className="container">
-        <TextForm heading="Enter Text Here" mode={mode} showAlert={showAlert}></TextForm>
+        <TextForm heading="Enter Text Here" mode={mode}></TextForm>
       </div>
       <About></About>
 
